0362a537172c3cb98646b9fceecd563ec1b9883f
Fabio Callegari <fabiocallegari.it@gmail.com>
2020-01-18 13:30:22 -0800

    change Details to be a class component

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,37 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = () => {
-  return <h1>Hi</h1>;
-};
+class Details extends React.Component {
+  state = { loading: true };
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    }, console.error);
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+    const { animal, breed, location, name, description } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
